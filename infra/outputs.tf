18267572a5b2c2796c26068a5af376e2b0ad0feb
Mohammad Sepahvand <mohammsep@gmail.com>
2026-03-01 22:28:32 +1100

    fix: reference existing GitHubActionsDeployRole instead of creating IAM resources

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,4 +1,4 @@
 output "role_arn" {
   description = "ARN of the IAM role that GitHub Actions should assume"
-  value       = aws_iam_role.github_actions.arn
+  value       = data.aws_iam_role.github_actions.arn
 }
