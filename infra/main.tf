c8fc96b1c000c2f9d7bb880e9eddaf7a60431956
Mohammad Sepahvand <mohammsep@gmail.com>
2026-03-01 20:20:35 +1100

    Extract hardcoded values to variables in Terraform

---
+++ infra/main.tf
@@ -0,0 +1,76 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+data "aws_caller_identity" "current" {}
+
+resource "aws_iam_openid_connect_provider" "github" {
+  url             = var.github_oidc_url
+  client_id_list  = [var.github_oidc_client_id]
+  thumbprint_list = [var.github_oidc_thumbprint]
+}
+
+# Policy document for trust relationship
+data "aws_iam_policy_document" "github_assume" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Federated"
+      identifiers = [aws_iam_openid_connect_provider.github.arn]
+    }
+
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+
+    condition {
+      test     = "StringEquals"
+      variable = "token.actions.githubusercontent.com:sub"
+      values   = ["repo:${var.github_repo}"]
+    }
+
+    dynamic "condition" {
+      for_each = var.github_branch != "" ? [1] : []
+      content {
+        test     = "StringLike"
+        variable = "token.actions.githubusercontent.com:ref"
+        values   = [var.github_branch]
+      }
+    }
+  }
+}
+
+resource "aws_iam_role" "github_actions" {
+  name               = var.iam_role_name
+  assume_role_policy = data.aws_iam_policy_document.github_assume.json
+}
+
+# Inline permissions policy
+data "aws_iam_policy_document" "github_policy" {
+  statement {
+    effect    = "Allow"
+    actions   = [
+      "ecr:DescribeRepositories",
+      "ecr:CreateRepository",
+      "ecr:GetAuthorizationToken",
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:PutImage",
+      "ecr:InitiateLayerUpload",
+      "ecr:UploadLayerPart",
+      "ecr:CompleteLayerUpload",
+      "ecr:ListImages"
+    ]
+    resources = ["*"]
+  }
+
+  statement {
+    effect    = "Allow"
+    actions   = ["apprunner:UpdateService"]
+    resources = ["arn:aws:apprunner:${var.aws_region}:${data.aws_caller_identity.current.account_id}:service/*"]
+  }
+}
+
+resource "aws_iam_role_policy" "github_actions_policy" {
+  name   = var.iam_policy_name
+  role   = aws_iam_role.github_actions.id
+  policy = data.aws_iam_policy_document.github_policy.json
+}
---
+++ infra/variables.tf
@@ -0,0 +1,46 @@
+variable "github_repo" {
+  description = "GitHub repository in the form owner/name"
+  type        = string
+}
+
+variable "github_branch" {
+  description = "Optional branch pattern to restrict OIDC tokens (e.g. refs/heads/master)"
+  type        = string
+  default     = ""
+}
+
+variable "aws_region" {
+  description = "AWS region to deploy into"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "github_oidc_url" {
+  description = "GitHub OIDC provider URL"
+  type        = string
+  default     = "https://token.actions.githubusercontent.com"
+}
+
+variable "github_oidc_client_id" {
+  description = "GitHub OIDC client ID (typically sts.amazonaws.com)"
+  type        = string
+  default     = "sts.amazonaws.com"
+}
+
+variable "github_oidc_thumbprint" {
+  description = "GitHub OIDC provider thumbprint"
+  type        = string
+  default     = "6938fd4d98bab03faadb97b34396831e3780aea1"
+}
+
+variable "iam_role_name" {
+  description = "Name of the IAM role for GitHub Actions"
+  type        = string
+  default     = "GitHubActionsDeployRole"
+}
+
+variable "iam_policy_name" {
+  description = "Name of the inline IAM policy"
+  type        = string
+  default     = "GitHubActionsECRAppRunner"
+}
