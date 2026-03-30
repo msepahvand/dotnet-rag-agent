9d394b26e1e3aa8d505478bc2272f827930ed35a
Mohammad Sepahvand <mohammsep@gmail.com>
2026-03-30 17:51:57 +1100

    fix: enable force delete for ecr repository teardown

--- infra/main.tf
+++ infra/main.tf
@@ -77,6 +77,7 @@ module "s3_vectors" {
 resource "aws_ecr_repository" "api" {
   name                 = var.ecr_repository_name
   image_tag_mutability = "IMMUTABLE"
+  force_delete         = true
 
   image_scanning_configuration {
     scan_on_push = true
